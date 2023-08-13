21bd3eab06ceb0f1c6e4149c424543a1acbcebb1
GEckett <grant.eckett@gmail.com>
2023-08-13 16:35:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ df = pandas.DataFrame(nato_alpha)
 df_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def word_entry():
+    try:
+        user_word = input("Enter a word").upper()
+        user_word_nato = [df_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        word_entry()
+    else:
+        print(user_word_nato)
 
-user_word = input("Enter a word").upper()
-user_word_nato = [df_dict[letter] for letter in user_word]
-print(user_word_nato)
+word_entry()
